69ae1e110a818000085ca3db35094243b87de82d
TiLied <TiLied@users.noreply.github.com>
2024-12-17 17:10:57 +0300

    Added a comment to GenerateOneFromStringAsync.

--- CSharpToJavaScript/Utils/Unsupported.cs
+++ CSharpToJavaScript/Utils/Unsupported.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CSharpToJavaScript.Utils
+﻿namespace CSharpToJavaScript.Utils
 {
 	public class Unsupported
 	{
+
 	}
 }

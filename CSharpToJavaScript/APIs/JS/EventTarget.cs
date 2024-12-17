470d865008a906e2d06af8a2ee1161fc29efedb5
TiLied <TiLied@users.noreply.github.com>
2024-12-17 17:15:00 +0300

    Updated generated xml docs. See: https://github.com/TiLied/GenDocsLib/commit/27e38fd26cb11ce1cbfc320a201a94690744d10d Updated generated csharp. See: https://github.com/TiLied/GenCSharpLib/commit/2f9d033dcdc7d34c351a1b602b3f19bac507bde6

--- CSharpToJavaScript/APIs/JS/EventTarget.cs
+++ CSharpToJavaScript/APIs/JS/EventTarget.cs
@@ -9,11 +9,11 @@ namespace CSharpToJavaScript.APIs.JS
 	public partial class EventTarget
 	{
 		
-		public GlobalObject.Undefined AddEventListener(string type, Action? callback, Union23 options) { throw new System.NotImplementedException(); }
-		public GlobalObject.Undefined AddEventListener(string type, Action<MouseEvent>? callback, Union23 options) { throw new System.NotImplementedException(); }
-		public GlobalObject.Undefined AddEventListener(string type, Action<Event>? callback, Union23 options) { throw new System.NotImplementedException(); }
+		public GlobalObject.Undefined AddEventListener(string type, Action? callback, Union27 options) { throw new System.NotImplementedException(); }
+		public GlobalObject.Undefined AddEventListener(string type, Action<MouseEvent>? callback, Union27 options) { throw new System.NotImplementedException(); }
+		public GlobalObject.Undefined AddEventListener(string type, Action<Event>? callback, Union27 options) { throw new System.NotImplementedException(); }
 
-		public GlobalObject.Undefined RemoveEventListener(string type, Action? callback, Union23 options) { throw new System.NotImplementedException(); }
+		public GlobalObject.Undefined RemoveEventListener(string type, Action? callback, Union28 options) { throw new System.NotImplementedException(); }
 	
 	}
 }

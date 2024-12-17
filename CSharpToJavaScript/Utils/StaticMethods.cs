1fdb7bfa16d46f16baea85ca5a654917112ab87c
TiLied <TiLied@users.noreply.github.com>
2024-12-17 17:10:34 +0300

    Colorful output to console) Added static keyword for methods and properties. Small fixes, formatting and "Trim"->"trim".

--- CSharpToJavaScript/Utils/StaticMethods.cs
+++ CSharpToJavaScript/Utils/StaticMethods.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 using System.IO;
 using System.Runtime.CompilerServices;
 
@@ -8,7 +9,17 @@ namespace CSharpToJavaScript.Utils
 	{
 		internal static void Log(string message, [CallerFilePath] string? file = null, [CallerMemberName] string? member = null, [CallerLineNumber] int line = 0)
 		{
+			if (message.StartsWith("---"))
+				Console.ForegroundColor = ConsoleColor.Green;
+
+			if (message.StartsWith("ERROR"))
+				Console.ForegroundColor = ConsoleColor.Red;
+
+			if (message.StartsWith("WARNING"))
+				Console.ForegroundColor = ConsoleColor.Yellow;
+
 			Trace.WriteLine($"({line}):{Path.GetFileName(file)} {member}: {message}");
+			Console.ResetColor();
 		}
 	}
 }

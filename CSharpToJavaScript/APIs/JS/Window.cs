242f25f5b1367d2b091b5376cea66a14594e144e
TiLied <TiLied@users.noreply.github.com>
2024-12-17 17:09:04 +0300

    Further improvement to "this". And some missing expressions.

---
+++ CSharpToJavaScript/APIs/JS/Window.cs
@@ -0,0 +1,18 @@
+﻿using CSharpToJavaScript.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpToJavaScript.APIs.JS
+{
+	public partial class Window
+	{
+		[To(ToAttribute.FirstCharToLowerCase)]
+		public dynamic Eval(string jsStr) 
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}

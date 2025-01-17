f2d3732df4953d0243ffa8269e0f9530ca0ccc58
TiLied <TiLied@users.noreply.github.com>
2025-01-17 22:06:32 +0300

    Added BigInt.

---
+++ CSharpToJavaScript/APIs/JS/Ecma/BigInt.cs
@@ -0,0 +1,42 @@
+﻿using CSharpToJavaScript.Utils;
+
+namespace CSharpToJavaScript.APIs.JS.Ecma;
+
+//https://262.ecma-international.org/14.0/#sec-bigint-objects
+[To(ToAttribute.Default)]
+public class BigInt : BigIntPrototype
+{
+	[To(ToAttribute.FirstCharToLowerCase)]
+	public static BigIntPrototype Prototype { get; } = new();
+
+	public BigInt(double value) { }
+
+	[To(ToAttribute.FirstCharToLowerCase)]
+	public BigInt AsIntN(Number bits, BigInt bigint) 
+	{
+		throw new System.NotImplementedException();
+	}
+
+	[To(ToAttribute.FirstCharToLowerCase)]
+	public BigInt AasUintN(Number bits, BigInt bigint)
+	{
+		throw new System.NotImplementedException();
+	}
+}
+public class BigIntPrototype 
+{
+	public BigIntPrototype() { }
+
+	public string ToLocaleString(dynamic? reserved1 = null, dynamic? reserved2 = null) 
+	{
+		throw new System.NotImplementedException();
+	}
+	public string ToString(Number? radix = null)
+	{
+		throw new System.NotImplementedException();
+	}
+	public BigInt ValueOf()
+	{
+		throw new System.NotImplementedException();
+	}
+}
